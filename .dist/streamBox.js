df08300573ac2b12aae26483ae79e1db0699fba1
Steven Renault <steven@MacBook-Air-de-Steven.local>
2020-06-19 17:51:34 +0200

    debut map 2

--- .dist/streamBox.js
+++ .dist/streamBox.js
@@ -17,37 +17,107 @@ function transformLine(str, index) {
   return "".concat(start, "v").concat(end);
 }
 
+function myWriteFile(filename, finalContent) {
+  fs.writeFile(filename, finalContent, {
+    encoding: 'utf8'
+  }, function (err) {
+    if (err) {
+      throw err;
+    } else {
+      console.log(_chalk["default"].green.bold("Successfully, you found the way from the path " + filename));
+    }
+  });
+}
+
 function drawExit(filename) {
   var rstream = fs.createReadStream(filename);
   var content = '';
   var finalContent = '';
   rstream.on('data', function (chunk) {
-    content += chunk.toString();
+    content += chunk.toString(); // console.log(content)
   });
-  rstream.on("end", function () {
-    var lines = content.split("\n");
-    var positionOne = lines[1].toString().indexOf('1');
-    var newLine = '';
 
-    for (var key in lines) {
-      if (lines[key] == lines[0] || lines[key] == lines[lines.length - 1]) {
-        finalContent += lines[key] + '\n';
-      } else {
-        newLine = transformLine(lines[key], positionOne);
-        finalContent += newLine + '\n';
-      }
-    }
+  if (filename === 'data/maps/oval.01.map') {
+    rstream.on("end", function () {
+      var lines = content.split("\n");
+      var positionOne = lines[1].toString().indexOf('1');
+      var newLine = '';
 
-    fs.writeFile(filename, finalContent, {
-      encoding: 'utf8'
-    }, function (err) {
-      if (err) {
-        throw err;
-      } else {
-        console.log(_chalk["default"].green.bold("Successfully, you found the way from the path " + filename));
+      for (var key in lines) {
+        if (lines[key] == lines[0] || lines[key] == lines[lines.length - 1]) {
+          finalContent += lines[key] + '\n';
+        } else {
+          newLine = transformLine(lines[key], positionOne);
+          finalContent += newLine + '\n';
+        }
       }
+
+      myWriteFile(filename, finalContent);
     });
-  });
+  } else {
+    rstream.on("end", function () {
+      var lines = content.split("\n");
+      var positionTwo = -1;
+      var positionLineTwo = -1;
+      var lineNumber = 27;
+      var column = 46;
+      var _char = '';
+      var newLine = '';
+
+      for (var key in lines) {
+        if (lines[key].includes('2')) {
+          positionTwo = lines[key].toString().indexOf('2');
+          positionLineTwo = parseInt(key);
+        }
+
+        if (!lines[key].includes(' ')) {
+          finalContent += lines[key] + '\n';
+        } else {
+          if (parseInt(key) <= 26) {
+            // console.log(lines[parseInt(key)+1])
+            if (lines[parseInt(key) + 1][positionTwo] === ' ') {
+              console.log(key);
+              newLine = lines[parseInt(key) + 1].toString().replace(' ', 'v');
+
+              if (lines[parseInt(key)][positionTwo + 1] === ' ') {
+                newLine = lines[parseInt(key) + 1].toString().replace(' ', '>');
+              }
+
+              finalContent += newLine + '\n'; // if (lines[parseInt(key)][positionTwo + 1] === ' ') {
+              //     newLine = lines[parseInt(key) + 1].toString().replace(' ', '>')
+              //     finalContent += newLine + '\n'
+              // }
+            }
+          }
+        } // if(lines[parseInt(key)+1][positionTwo] === 'x'){
+        //     console.log(lines[key])
+        // }
+        //    if(lines[positionLineTwo+1][positionTwo] === "x"){
+        //    }
+        // console.log(lines[key])
+        // if (lines[key].includes('1')) {
+        //     positionOne = lines[key].toString().indexOf('1')
+        //     // console.log("positionOne")
+        //     for (let i = positionOne; i > 0; i--) {
+        //         // console.log(lines[key][i-1])
+        //         // if(lines[key][i-1] == ' '){
+        //         //     char+= lines[key][i-1].replace(' ',"<")
+        //         // }
+        //         // char+= lines[key][i].replace(' ',"<")
+        //         // char+= lines[key][i].replace(' ',"<")
+        //         // if (lines[key][i] == ' ') {
+        //         //     newLine = lines[key][i].replace(' ', "<")
+        //         //     finalContent += newLine + '\n'
+        //         // }
+        //     }
+        //     // console.log(lines[parseInt(key)+1][positionOne])
+        // }
+
+      }
+
+      console.log(finalContent); // console.log(positionTwo);
+    });
+  }
 }
 
 module.exports = {
--- src/streamBox.ts
+++ src/streamBox.ts
@@ -10,7 +10,15 @@ function transformLine(str: string, index: number): string {
     let start: string = str.slice(0, index)
     let end: string = str.slice(index, str.length)
     return `${start}v${end}`
+}
 
+function myWriteFile(filename: string, finalContent: string) {
+    fs.writeFile(filename, finalContent, { encoding: 'utf8' }, (err: string) => {
+        if (err) { throw err }
+        else {
+            console.log(chalk.green.bold("Successfully, you found the way from the path " + filename))
+        }
+    });
 }
 
 function drawExit(filename: string): void {
@@ -22,28 +30,82 @@ function drawExit(filename: string): void {
 
     rstream.on('data', (chunk: object) => {
         content += chunk.toString()
+        // console.log(content)
     })
 
-    rstream.on("end", () => {
-        let lines: Array<string> = content.split("\n")
-        let positionOne: number = lines[1].toString().indexOf('1')
-        let newLine: string  = ''
-        for (const key in lines) {
-            if (lines[key] == lines[0] || lines[key] == lines[lines.length - 1]) {
-                finalContent += lines[key] + '\n'
-            } else {
-                newLine = transformLine(lines[key], positionOne)
-                finalContent += newLine + '\n'
+    if (filename === 'data/maps/oval.01.map') {
+        rstream.on("end", () => {
+            let lines: Array<string> = content.split("\n")
+            let positionOne: number = lines[1].toString().indexOf('1')
+            let newLine: string = ''
+            for (const key in lines) {
+                if (lines[key] == lines[0] || lines[key] == lines[lines.length - 1]) {
+                    finalContent += lines[key] + '\n'
+                } else {
+                    newLine = transformLine(lines[key], positionOne)
+                    finalContent += newLine + '\n'
+                }
             }
-        }
+            myWriteFile(filename, finalContent)
+        })
+    }
 
-        fs.writeFile(filename, finalContent, { encoding: 'utf8' }, (err: string) => {
-            if (err) { throw err }
-            else {
-                console.log(chalk.green.bold("Successfully, you found the way from the path " + filename))
+    else {
+
+
+        rstream.on("end", () => {
+            let lines: Array<string> = content.split("\n")
+            let positionTwo: number = -1
+            let positionLineTwo: number = -1
+            let lineNumber: number = 27
+            let column: number = 46
+            let char: string = ''
+            let newLine: string = ''
+
+
+            for (const key in lines) {
+
+
+                if (lines[key].includes('2')) {
+                    positionTwo = lines[key].toString().indexOf('2')
+                    positionLineTwo = parseInt(key)
+
+                }
+                if (!lines[key].includes(' ')) {
+                    finalContent += lines[key] + '\n'
+                } else {
+                    if (parseInt(key) <= 26) {
+                        // console.log(lines[parseInt(key)+1])
+                        if (lines[parseInt(key) + 1][positionTwo] === ' ') {
+                            console.log(key)
+                            newLine = lines[parseInt(key) + 1].toString().replace(' ', 'v')
+
+                            if(lines[parseInt(key)][positionTwo+1] === ' '){
+                                newLine = lines[parseInt(key) + 1].toString().replace(' ', '>')
+                            }
+                            finalContent += newLine + '\n'
+
+                            // if (lines[parseInt(key)][positionTwo + 1] === ' ') {
+                            //     newLine = lines[parseInt(key) + 1].toString().replace(' ', '>')
+                            //     finalContent += newLine + '\n'
+                            // }
+                        }
+
+                    }
+
+                }
+
             }
-        });
-    })
+            console.log(finalContent);
+            // console.log(positionTwo);
+
+
+
+
+        })
+
+
+    }
 
 }
 
